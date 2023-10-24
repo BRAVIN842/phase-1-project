dd03f8076cabf742f0adc52b4daef4a3600439b6
Bravin <onsasebravin853@gmail.com>
2023-10-24 14:05:52 +0300

    JS variables

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    const amiiboContainer = document.getElementById("amiibo-container");
+    const searchInput = document.getElementById("search-input");
+    const filterSelect = document.getElementById("filter-select");
+})
