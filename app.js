f8e362a965369a69e0472bf7a20aa102e8a31d76
Bravin <onsasebravin853@gmail.com>
2023-10-24 14:08:29 +0300

    JS fetch

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     const amiiboContainer = document.getElementById("amiibo-container");
     const searchInput = document.getElementById("search-input");
     const filterSelect = document.getElementById("filter-select");
+
+    //Fetch data from API
+    fetch("http://localhost:3000/amiibo")
 })
